29a80c0d2fb30e6c226e7889cd3dcb189ac42ece
Ritvars Biksis <ritvars@ritvars.eu>
2017-12-13 11:17:12 +0200

    :bug: Fixed Webpack prod conf.

--- src/index.js
+++ src/index.js
@@ -12,11 +12,7 @@ import { helloSaga } from './sagas'
 
 const theme = createMuiTheme()
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(sagaMiddleware)
-)
+const store = createStore(reducers, {}, applyMiddleware(sagaMiddleware))
 
 sagaMiddleware.run(helloSaga)
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   plugins: [
     new UglifyJSPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
     })
   ]
 })
